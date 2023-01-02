c8278b4e5793521d6ca981e8f58b4f781124b3f2
emilwandersen <emilwandersen@gmail.com>
2023-01-02 15:34:37 +0100

    Boolean legal spots

--- Board.java
+++ Board.java
@@ -8,41 +8,39 @@ public class Board {
 
     int[][] map;
 
-    public void Board(int y_axis, int x_axis){
-        this.y_axis=y_axis;
-        this.x_axis=x_axis;
-        this.map=new int[x_axis][y_axis];
-    }
-    public void Board(int size){
-        this.y_axis=size;
-        this.x_axis=size;
-        map =new int[size][size];
+    public void Board(int y_axis, int x_axis) {
+        this.y_axis = y_axis;
+        this.x_axis = x_axis;
+        this.map = new int[x_axis][y_axis];
     }
 
-    public void iniGame(){
-        map[x_axis/2][y_axis/2]=1;
-        map[x_axis/2+1][y_axis/2]=2;
-        map[x_axis/2+1][y_axis/2+1]=1;
-        map[x_axis/2][y_axis/2+1]=2;
+    public void Board(int size) {
+        this.y_axis = size;
+        this.x_axis = size;
+        map = new int[size][size];
+    }
+
+    public void iniGame() {
+        map[x_axis / 2][y_axis / 2] = 1;
+        map[x_axis / 2 + 1][y_axis / 2] = 2;
+        map[x_axis / 2 + 1][y_axis / 2 + 1] = 1;
+        map[x_axis / 2][y_axis / 2 + 1] = 2;
     }
 
 
-    public void legalSpots(int playerTurn){
+    public boolean legalSpots(int playerTurn) {
 
 
-        for (int i = 0; i<x_axis;i++){
-            for (int j = 0; j<y_axis;j++){
-                if(map[i][j]==0){
+        for (int i = 0; i < x_axis; i++) {
+            for (int j = 0; j < y_axis; j++) {
+                if (map[i][j] == 0) {
 
-                }else;
+
+                } else ;
             }
         }
+        return true;
     }
-
-
-
-
-
 
 
 }
