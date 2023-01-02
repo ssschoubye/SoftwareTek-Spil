a1a91145b2c100697e03a5c731ef17b588fc6f01
emilwandesen <emilwandersen@gmail.com>
2023-01-03 00:06:37 +0100

    Better(more dry) legalSpots, placePiece and BoardTester

--- Board.java
+++ Board.java
@@ -21,14 +21,14 @@ public class Board {
         map = new int[size][size];
     }
 
-    public void iniGame() {
+    public void initialize() {
         map[x_axis / 2 - 1][y_axis / 2 - 1] = 1;
         map[x_axis / 2][y_axis / 2 - 1] = 2;
         map[x_axis / 2][y_axis / 2] = 1;
         map[x_axis / 2 - 1][y_axis / 2] = 2;
     }
 
-
+    /*
     public boolean legalSpots(int playerTurn) {
         boolean anyLegalSpots = false;
         for (int x = 0; x < x_axis; x++) {
@@ -174,119 +174,109 @@ public class Board {
         }
         return anyLegalSpots;
     }
+    */
+
+    public boolean legalSpots(int playerTurn) {
+        boolean anyLegalSpots = false;
+        for (int x = 0; x < x_axis; x++) {
+            for (int y = 0; y < y_axis; y++) {
+                if (map[x][y] == 3) {
+                    map[x][y] = 0;
+                }
+                if (map[x][y] != 0) continue;
+                for (int dx = -1; dx <= 1; dx++) {
+                    for (int dy = -1; dy <= 1; dy++) {
+                        if (dx == 0 && dy == 0) {
+                            continue;
+                        }
+                        if (!isOnBoard(x + dx, y + dy)) {
+                            continue;
+                        }
+                        if (map[x + dx][y + dy] != 3 - playerTurn) {
+                            continue;
+                        }
+
+                        int l = 0;
+
+                        while (true) {
+                            l++;
+                            if (!isOnBoard(x + dx * l, y + dy * l)) {
+                                break;
+                            }
+                            if (map[x + dx * l][y + dy * l] == playerTurn) {
+                                map[x][y] = 3;
+                                anyLegalSpots = true;
+                                break;
+                            } else if (map[x + dx * l][y + dy * l] != 3 - playerTurn) {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return anyLegalSpots;
+    }
 
 
-    public boolean placePiece(int x, int y, int playerTurn){
+    public boolean isOnBoard(int x, int y) {
+        return x >= 0 && y >= 0 && x < x_axis && y < y_axis;
 
-        if (playerTurn!=1 && playerTurn!=2){
+    }
+
+
+    public boolean placePiece(int x, int y, int playerTurn) {
+
+        if (playerTurn != 1 && playerTurn != 2) {
             System.out.println("Value has to be 1 or 2");
             return false;
         }
-        if (x<0 ||x>=x_axis || y<0 || y>=y_axis|| map[x][y] != 3) {
+        if (x < 0 || x >= x_axis || y < 0 || y >= y_axis || map[x][y] != 3) {
             System.out.println("Not possible placement");
             return false;
         }
 
-        map[x][y]=playerTurn;
+        map[x][y] = playerTurn;
 
         //Checking which pieces to flip
-        //Compare up left
-        if ((x > 0 && y > 0) && map[x - 1][y - 1] == 3 - playerTurn) {
-            int checks = min(x - 1, y - 1);
-            for (int l = 1; l <= checks; l++) {
-                if (map[x - 1 - l][y - 1 - l] == playerTurn) {
-
-                } else if (map[x - 1 - l][y - 1 - l] != 3 - playerTurn) {
-                    break;
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                if (dx == 0 && dy == 0) {
+                    continue;
                 }
+                if (!isOnBoard(x + dx, y + dy)) {
+                    continue;
+                }
+                if (map[x + dx][y + dy] != 3 - playerTurn) {
+                    continue;
+                }
+                boolean flipDirection = false;
+                int l = 0;
+
+                while (true) {
+                    l++;
+                    if (!isOnBoard(x + dx * l, y + dy * l)) {
+                        break;
+                    }
+                    if (map[x + dx * l][y + dy * l] == playerTurn) {
+                        flipDirection = true;
+                        break;
+                    } else if (map[x + dx * l][y + dy * l] != 3 - playerTurn) {
+                        break;
+                    }
+                }
+                if (flipDirection){
+                    while (l > 0) {
+                        l--;
+                        map[x + dx * l][y + dy * l] = playerTurn;
+
+                    }
+                }
+
+
+
             }
         }
-        //Compare up
-        if ((y > 0) && map[x][y - 1] == 3 - playerTurn) {
-            int checks = y - 1;
-            for (int l = 1; l <= checks; l++) {
-                if (map[x][y - 1 - l] == playerTurn) {
-
-                } else if (map[x][y - 1 - l] != 3 - playerTurn) {
-                    break;
-                }
-            }
-        }
-
-        //Compare up right
-        if ((x < x_axis - 1 && y > 0) && map[x + 1][y - 1] == 3 - playerTurn) {
-            int checks = min(x_axis - x - 2, y - 1);
-            for (int l = 1; l <= checks; l++) {
-                if (map[x + 1 + l][y - 1 - l] == playerTurn) {
-
-                } else if (map[x + 1 + l][y - 1 - l] != 3 - playerTurn) {
-                    break;
-                }
-
-            }
-        }
-
-        //Compare left
-        if (x > 0 && map[x - 1][y] == 3 - playerTurn) {
-            int checks = x - 1;
-            for (int l = 1; l <= checks; l++) {
-                if (map[x - 1 - l][y] == playerTurn) {
-
-                } else if (map[x - 1 - l][y] != 3 - playerTurn) {
-                    break;
-                }
-            }
-        }
-
-        //Compare right
-        if (x < x_axis - 1 && map[x + 1][y] == 3 - playerTurn) {
-            int checks = x_axis - x - 2;
-            for (int l = 1; l <= checks; l++) {
-                if (map[x + 1 + l][y] == playerTurn) {
-
-                } else if (map[x + 1 + l][y] != 3 - playerTurn) {
-                    break;
-                }
-            }
-        }
-
-        //Compare down left
-        if ((x > 0 && y < y_axis - 1) && map[x - 1][y + 1] == 3 - playerTurn) {
-            int checks = min(x - 1, y_axis - y - 2);
-            for (int l = 1; l <= checks; l++) {
-                if (map[x - 1 - l][y + 1 + l] == playerTurn) {
-
-                } else if (map[x - 1 - l][y + 1 + l] != 3 - playerTurn) {
-                    break;
-                }
-            }
-
-        }
-
-        //Compare down
-        if (y < y_axis - 1 && map[x][y + 1] == 3 - playerTurn) {
-            int checks = y_axis - y - 2;
-            for (int l = 0; l <= checks; l++) {
-                if (map[x][y + 1 + l] == playerTurn) {
-
-                } else if (map[x][y + 1 + l] != 3 - playerTurn) {
-                    break;
-                }
-            }
-        }
-
-        //Compare down left
-        if ((x < x_axis - 1 && y < y_axis - 1) && map[x + 1][y + 1] == 3 - playerTurn) {
-            int checks = min(x_axis - x - 2, y_axis - y - 2);
-            for (int l = 1; l <= checks; l++) {
-                if (map[x + 1 + l][y + 1 + l] == playerTurn) {
-
-                } else if (map[x + 1 + l][y + 1 + l] != 3 - playerTurn) {
-                    break;
-                }
-            }
-        }
-
 
 
         return true;
@@ -302,13 +292,41 @@ public class Board {
 
     }
 
-
+    public int[] getScore(){
+        int[] score = {0,0};
+        for (int x = 0; x<x_axis;x++){
+            for (int y = 0; y<y_axis;y++){
+                if (map[x][y]==1){
+                    score[0]++;
+                } else if (map[x][y]==2){
+                    score[1]++;
+                }
+            }
+        }
+        return score;
+    }
     //print the whole board using toString (ONLY FOR TESTING)
     @Override
     public String toString() {
+        System.out.print("    ");
+        for (int x=0;x<x_axis;x++){
+            System.out.print(x+" ");
+        }
+        System.out.print("\n   _");
+        for (int x=0;x<x_axis;x++){
+            System.out.print(" _");
+        }
+        System.out.println();
         for (int i = 0; i < y_axis; i++) {
+            System.out.print(i+" | ");
             for (int j = 0; j < x_axis; j++) {
-                System.out.print(map[j][i] + " ");
+                if (map[j][i]==0){
+                    System.out.print("- ");
+                } else if (map[j][i]==3) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(map[j][i] + " ");
+                }
             }
             System.out.println();
         }
--- BoardTestMain.java
+++ BoardTestMain.java
@@ -1,22 +1,112 @@
-
+import java.util.Scanner;
 
 public class BoardTestMain {
+
+    public static int turnSwitch(int currentTurn){
+        if (currentTurn==1){
+            return 2;
+        } else if (currentTurn==2){
+            return 1;
+        } else {
+            System.out.println("Wrong current turn");
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
+        /*
+        Board testGame = new Board(8,8);
 
-        Board game = new Board(8,8);
+        testGame.initialize();
+        testGame.legalSpots(2);
+        testGame.toString();
 
-        game.iniGame();
-        game.setTestValue(3,5,1);
+        System.out.println("\n____________________________________");
 
+        testGame.placePiece(3,2,2);
+        testGame.legalSpots(1);
+        testGame.toString();
 
-        game.legalSpots(2);
+        System.out.println("\n____________________________________");
+
+        testGame.placePiece(2,4,1);
+        testGame.legalSpots(2);
+        testGame.toString();
+
+        System.out.println("\n____________________________________");
+
+        testGame.placePiece(2,4,1);
+        testGame.legalSpots(2);
+        testGame.toString();
+
+        System.out.println("\n____________________________________");
+        System.out.println("Test over");
+        System.out.println("Now starting game");
+        System.out.println("\n____________________________________");
+        */
+
+        Scanner size = new Scanner(System.in);
+        System.out.print("Enter the size of the x-axis: ");
+        int xSize = size.nextInt();
+        System.out.print("Enter the size of the y-axis: ");
+        int ySize = size.nextInt();
+
+        Board game = new Board(xSize,ySize);
+
+        game.initialize();
+        //Game starts with 1 (White) having the first turn
+        int turn=2;
+        System.out.println("\n____________________________________");
+        while (true){
+            turn = turnSwitch(turn);
+
+            if(!game.legalSpots(turnSwitch(turn)) && !game.legalSpots(turn)){
+                System.out.println("No more possible moves \n    game over");
+                break;
+            }
+            if(!game.legalSpots(turn)){
+                System.out.println("\n"+turn+" has no possible moves");
+                continue;
+            }
+            game.toString();
+
+            System.out.println("\n"+turn+"'s turn to place a piece");
+
+            Scanner coord = new Scanner(System.in);
+
+            int x=0;
+            int y=0;
+
+            while (true){
+                System.out.print("Enter the first coordinate: ");
+                x = coord.nextInt();
+                System.out.print("Enter the second coordinate: ");
+                y = coord.nextInt();
+                if (game.placePiece(x,y,turn)){
+                    break;
+                }
+
+            }
+            System.out.println("\n____________________________________");
+
+        }
+        System.out.println("\n____________________________________");
+        int[] score = game.getScore();
         game.toString();
-        System.out.println("------------------");
-        game.setTestValue(4,5,2);
 
-        game.legalSpots(1);
+        if (score[0]>score[1]){
+            System.out.println("1 won the game with " + score[0] + " pieces on the board.");
+            System.out.println("2 only had " + score[1] + " pieces.");
+        } else if (score[0]<score[1]){
+            System.out.println("2 won the game with " + score[1] + " pieces on the board.");
+            System.out.println("1 only had " + score[0] + " pieces.");
+        } else if (score[0]==score[1]){
+            System.out.println("The game was a draw. Both players had" + score[0] + " pieces on the board.");
 
-        game.toString();
+        } else {
+            System.out.println("ERROR");
+        }
+
 
 
     }
