dd66552dfdc903d61e71dbdc5fcf7660858a7b31
emilwandesen <emilwandersen@gmail.com>
2023-01-03 22:09:41 +0100

    BoardFix

--- Board.java
+++ Board.java
@@ -22,14 +22,6 @@ public class Board {
     }
 
     public void initialize() {
-        //Test
-        /*
-        map[0][0] = 1;
-        map[1][0] = 2;
-        map[0][1] = 2;
-        map[1][1] = 2;
-         */
-
         map[x_axis / 2 - 1][y_axis / 2 - 1] = 1;
         map[x_axis / 2][y_axis / 2 - 1] = 2;
         map[x_axis / 2][y_axis / 2] = 1;
