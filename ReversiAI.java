0cf28cfe2b73f89c775c7e39d7c88992cc2f178d
FrederikvSvane <s224766@student.dtu.dk>
2023-01-08 19:11:55 +0100

    Clever AI version 2.1

--- ReversiAI.java
+++ ReversiAI.java
@@ -11,30 +11,49 @@ public class ReversiAI {
         }
 
 
-        public static void AIMakeMove(int playerTurn){
+        public static void AIMakeMove(int playerTurn) {
 
             int bestX = 0;
             int bestY = 0;
             int alpha = Integer.MIN_VALUE;
             int beta = Integer.MAX_VALUE;
             int score = 0;
-            for (int x = 0; x < board.x_axis; x++) {
-                for (int y = 0; y < board.y_axis; y++) {
-                    if (board.map[x][y] != 3 && board.map[x][y] != 4) continue;
-                    Board copy = board.copy();
-                    copy.placePiece(x, y, playerTurn);
-                    int value = minValue(copy, playerTurn, 1, alpha, beta);
-                    if (value > alpha) {
-                        alpha = value;
-                        bestX = x;
-                        bestY = y;
-                    }
-                }
+
+            if (board.map[0][0] == 3){
+                board.placePiece(0, 0, playerTurn);
+
+            }else if(board.map[board.x_axis-1][board.y_axis-1] == 3){
+                    board.placePiece(board.x_axis-1,board.y_axis-1,playerTurn);
+
+                }else if (board.map[board.x_axis-1][0] == 3){
+                        board.placePiece(board.x_axis-1,0,playerTurn);
+
+                     }else if(board.map[0][board.y_axis-1] == 3){
+                            board.placePiece(0,board.y_axis-1,playerTurn);
             }
 
-            // Place a piece on the best spot
-            board.placePiece(bestX, bestY, playerTurn);
+                else {
+
+                for (int x = 0; x < board.x_axis; x++) {
+                    for (int y = 0; y < board.y_axis; y++) {
+                        if (board.map[x][y] != 3 && board.map[x][y] != 4) continue;
+                        Board copy = board.copy();
+                        copy.placePiece(x, y, playerTurn);
+                        int value = minValue(copy, playerTurn, 1, alpha, beta);
+                        if (value > alpha) {
+                            alpha = value;
+                            bestX = x;
+                            bestY = y;
+                        }
+                    }
+                }
+
+                // Place a piece on the best spot
+                board.placePiece(bestX, bestY, playerTurn);
+
+            }
         }
+
 
     private static int maxValue(Board board, int playerTurn, int depth, int alpha, int beta) {
         if (depth == maxDepth || !board.legalSpots(playerTurn)) {
