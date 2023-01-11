153715ac7d3290af390580609f2ddb3ef420a8e2
ssschoubye <116802763+ssschoubye@users.noreply.github.com>
2023-01-11 18:30:34 +0100

    PromptHostUpdate
    Serverclient bliver nu kaldt i

--- ServerClient.java
+++ ServerClient.java
@@ -7,12 +7,43 @@ import java.util.Scanner;
 
 
 public class ServerClient {
-        public static void main(String[] args) throws IOException, ClassNotFoundException {
+        public static void runServerClient(String host) throws IOException, ClassNotFoundException {
             InetAddress localHost = InetAddress.getLocalHost();
             String client = localHost.getHostAddress();
-            // Connect to the server
-            //Socket socket = new Socket("192.168.1.8", 8080);
-            String host;
+
+            Socket socket = new Socket(host, 8080);
+            // Get the input and output streams
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(client);
+            while(true){
+                ObjectInputStream inObject = new ObjectInputStream(socket.getInputStream());
+
+                ArrayReturn boardRec = (ArrayReturn) inObject.readObject();
+                int[][] inputMap = boardRec.getArray();
+                //System.out.println(inputMap);
+                System.out.println(Arrays.deepToString(inputMap));
+
+                // Send a message to the server
+                System.out.println("What is your message?");
+                Scanner scan = new Scanner(System.in);
+                String message = scan.nextLine();
+                out.println(message);
+
+                String response = in.readLine();
+                // Read and process the server's response
+                if(response == "no"){
+                    break;
+                }
+                System.out.println("Response: " + response);  // prints "Response: ACK"
+            }
+            // Close the socket
+            socket.close();
+        }
+    }
+// Connect to the server
+//Socket socket = new Socket("192.168.1.8", 8080);
+
             /*
             try{
                 for (int i = 0; i <= 255; i++) {
@@ -46,37 +77,5 @@ public class ServerClient {
                 e.printStackTrace();
             }
              */
-            host = "192.168.1.10";
-            Socket socket = new Socket(host, 8080);
-            // Get the input and output streams
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            out.println(client);
-            while(true){
-                ObjectInputStream inObject = new ObjectInputStream(socket.getInputStream());
-
-                ArrayReturn boardRec = (ArrayReturn) inObject.readObject();
-                int[][] inputMap = boardRec.getArray();
-                //System.out.println(inputMap);
-                System.out.println(Arrays.deepToString(inputMap));
-
-                // Send a message to the server
-                System.out.println("What is your message?");
-                Scanner scan = new Scanner(System.in);
-                String message = scan.nextLine();
-                out.println(message);
-
-                String response = in.readLine();
-                // Read and process the server's response
-                if(response == "no"){
-                    break;
-                }
-                System.out.println("Response: " + response);  // prints "Response: ACK"
-            }
-            // Close the socket
-            socket.close();
-        }
-    }
-
 
 
