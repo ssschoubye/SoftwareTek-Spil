f14a9aed6927e8c824aa9b663f88b55abfde1053
emilwandesen <emilwandersen@gmail.com>
2023-01-16 23:11:39 +0100

    :)

--- BoardTestMain.java
+++ BoardTestMain.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class BoardTestMain {
 
@@ -12,39 +13,59 @@ public class BoardTestMain {
         }
     }
 
-    public static void gameRun() {
+    public static void main(String[] args) {
+        int turnCounter=1;
+        Scanner size = new Scanner(System.in);
+        System.out.print("Enter the size of the x-axis: ");
+        int xSize = size.nextInt();
+        System.out.print("Enter the size of the y-axis: ");
+        int ySize = size.nextInt();
 
-
-        int xSize = 8;
-        int ySize = 8;
         Board game = new Board(xSize, ySize);
+
         game.initialize();
-
         //Game starts with 1 (White) having the first turn
-        int turn = 2;
+        int turn = 1;
+        System.out.println("\n____________________________________");
         while (true) {
-            turn = turnSwitch(turn);
+            if (turnCounter>4){
+                turn = turnSwitch(turn);
 
-            if (!game.legalSpots(turn)) {
-                if(!game.legalSpots(turnSwitch(turn))){
-                    System.out.println("No more possible moves \n    game over");
-                    break;
-                } else{
-                    System.out.println("\n" + turn + " has no possible moves");
-                    continue;
+                if (!game.legalSpots(turn)) {
+                    if(!game.legalSpots(turnSwitch(turn))){
+                        System.out.println("No more possible moves \n    game over");
+                        break;
+                    } else{
+                        System.out.println("\n" + turn + " has no possible moves");
+                        continue;
+                    }
+
                 }
-
-            }
-            System.out.println();
-            game.printOut();
-
-            System.out.println("\n" + turn + "'s turn to place a piece");
+                System.out.println();
 
 
+                System.out.println("\n" + turn + "'s turn to place a piece");
 
+            } else if (turnCounter==3) {
+                turn = turnSwitch(turn);
+            }game.printOut();
+
+
+            Scanner coord = new Scanner(System.in);
+
+            int x = 0;
+            int y = 0;
+
+            do {
+                System.out.print("Enter the first coordinate: ");
+                x = coord.nextInt();
+                System.out.print("Enter the second coordinate: ");
+                y = coord.nextInt();
+
+            } while (!game.placePiece(x, y, turn));
 
             System.out.println("\n____________________________________");
-
+            turnCounter++;
         }
         System.out.println("\n____________________________________");
         int[] score = game.getScore();
