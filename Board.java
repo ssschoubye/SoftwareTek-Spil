8f756994c05bd1b02a8734eb3cae1244b2721c28
emilwandesen <emilwandersen@gmail.com>
2023-01-18 12:12:38 +0100

    Yepper

--- Board.java
+++ Board.java
@@ -3,32 +3,28 @@
 public class Board {
 
 
-    //Every game will have a certain size
+    //Every game will have a certain size consisting of an x- and y-axis
     public int x_axis;
     public int y_axis;
 
+    //Every game will have a 2d int array symbolizing the game state
     int[][] map;
 
-    static int gamemode;
 
-
+    //An object for initializing the object
     public Board(int x_axis, int y_axis) {
         this.y_axis = y_axis;
         this.x_axis = x_axis;
         this.map = new int[x_axis][y_axis];
     }
 
-    public Board(int size) {
-        this.y_axis = size;
-        this.x_axis = size;
-        map = new int[size][size];
-    }
-
+    //Constructor for the server
     public Board() {
-        //Constructor der bliver benyttet i Server.
         int[][] map;
     }
 
+    //Method for initializing the game board itself
+    //This is done by setting all four middle fields to 4 to indicate where the first four moves can be made
     public void initialize() {
         map[x_axis / 2 - 1][y_axis / 2 - 1] = 4;
         map[x_axis / 2][y_axis / 2 - 1] = 4;
@@ -37,25 +33,43 @@ public class Board {
 
     }
 
+    //Legal spots shows where a given player can make a move and returns whether of not the player actually has an available move
     public boolean legalSpots(int playerTurn) {
+        //Firsts sets that there is no legal spots (As the method is yet to disprove this)
         boolean anyLegalSpots = false;
+
+        //Nested for loop going through all fields in the map of the game
         for (int x = 0; x < x_axis; x++) {
             for (int y = 0; y < y_axis; y++) {
+
+                //Any previous indications of a possible player move is reverted to 0
                 if (map[x][y] == 3) {
                     map[x][y] = 0;
                 }
+
+                //If the field in the map isn't empty, move on to the next field
                 if (map[x][y] != 0) continue;
+
+                //nested for loop looking at the 8 fields around the actual field
                 for (int dx = -1; dx <= 1; dx++) {
                     for (int dy = -1; dy <= 1; dy++) {
+
+                        //checks if not supposed to look through a direction
+                        //(Does so if the piece looked at is opposite color of the current player)
                         if (checkXDxYDy(x, y, playerTurn, dx, dy)) continue;
 
+                        //initializes an int for moving further down a direction
                         int l = 0;
 
+                        //While loop checking further down a direction
                         while (true) {
                             l++;
+                            //If the coordinate set is not on the game board, the loop breaks
                             if (!isOnBoard(x + dx * l, y + dy * l)) {
                                 break;
                             }
+                            //If a piece of the color of the current player is found, the initial field is a possible move
+                            //In this case,
                             if (map[x + dx * l][y + dy * l] == playerTurn) {
                                 map[x][y] = 3;
                                 anyLegalSpots = true;
@@ -217,132 +231,6 @@ public class Board {
             if (y_axis >= 0) System.arraycopy(map[x], 0, copy.map[x], 0, y_axis);
         }
         return copy;
-    }
-
-    public static int boardHeuristic(Board board){
-
-        //Parameters
-        int whiteCoins = 0;
-        int blackCoins = 0;
-        int whiteStability = 0;
-        int blackStability = 0;
-        int whiteMobility;
-        int blackMobility;
-        int whiteCorners = 0;
-        int blackCorners = 0;
-
-        //Parameter weights
-        int coinsWeight = 25;
-        int cornerWeight = 500;
-        int stabilityWeight = 30;
-        int mobilityWeight = 5;
-
-        //Scores
-        int coinScore = 0;
-        int mobilityScore = 0;
-        int stabilityScore = 0;
-        int cornerScore = 0;
-        int score;
-
-        //Runs through all cells on board and counts parameters
-        for(int x=0; x<board.x_axis; x++){
-            for(int y=0; y<board.y_axis; y++){
-                if(board.map[x][y]==1){
-                    whiteCoins++;
-                    if(isStable(board, x, y, 1)){
-                        whiteStability++;
-                    }
-                    if((x == 0 || x == board.x_axis - 1) && (y == 0 || y == board.y_axis - 1)){
-                        whiteCorners++;
-                    }
-                }
-
-                if(board.map[x][y]==2){
-                    blackCoins++;
-                    if(isStable(board, x, y, 2)){
-                        blackStability++;
-                    }
-                    if((x == 0 || x == board.x_axis - 1) && (y == 0 || y == board.y_axis - 1)){
-                        blackCorners++;
-                    }
-                }
-            }
-        }
-
-        whiteMobility = getMobility(board,1);
-        blackMobility = getMobility(board,2);
-
-
-        if(whiteCoins+blackCoins != 0) {
-            coinScore = 100 * (whiteCoins - blackCoins) / (whiteCoins + blackCoins);
-        }
-
-        if(whiteMobility+blackMobility != 0) {
-            mobilityScore = 100 * (whiteMobility - blackMobility) / (whiteMobility + blackMobility);
-        }
-
-        if(whiteCorners+blackCorners != 0) {
-            cornerScore = 100 * (whiteCorners - blackCorners) / (whiteCorners + blackCorners);
-        }
-
-        if(whiteStability+blackStability != 0) {
-            stabilityScore = 100 * (whiteStability - blackStability) / (whiteStability + blackStability);
-        }
-
-
-        score = coinsWeight * coinScore + mobilityWeight * mobilityScore + cornerWeight * cornerScore + stabilityWeight * stabilityScore;
-
-        return score;
-    }
-
-
-    //The term mobility refers to the amount of possible moves a player has. The method below returns this number for a given player
-    private static int getMobility(Board board, int playerTurn){
-        int mobility = 0;
-        Board copy = board.copy();
-        copy.legalSpots(playerTurn);
-        for(int i=0; i<copy.x_axis; i++){
-            for(int j=0; j<copy.y_axis; j++){
-                if(copy.map[i][j]==3){
-                    mobility++;
-                }
-            }
-        }
-        return mobility;
-    }
-
-    private static boolean isStable(Board board, int x, int y, int piece) {
-
-        if(!stableDirection(board,x,y,-1,-1,piece)) return false;
-        if(!stableDirection(board,x,y,0,-1,piece)) return false;
-        if(!stableDirection(board,x,y,-1,0,piece)) return false;
-        return stableDirection(board, x, y, 1, -1, piece);
-
-    }
-
-    public static boolean stableDirection(Board board,int x,int y, int dx, int dy, int piece){
-        int l = 1;
-        int end1;
-        int end2 = 10;
-        while (board.isOnBoard(x + l * dx, y + l * dy) && board.map[x + l * dx][y + l * dy] == piece) {
-
-            l++;
-        }
-        if (board.isOnBoard(x + l * dx, y + l * dy)) {
-            end1 = board.map[x + l * dx][y + l * dy];
-
-            dx *= -1;
-            dy *= -1;
-            l = 1;
-            while (board.isOnBoard(x + l * dx, y + l * dy) && board.map[x + l * dx][y + l * dy] == piece) {
-                l++;
-            }
-            if (board.isOnBoard(x + l * dx, y + l * dy)) {
-                end2 = board.map[x + l * dx][y + l * dy];
-            }
-            return end2 == 10 || end1 == end2;
-        }
-        return true;
     }
 
     public int[][] getArray() {
--- MiniMaxAlphaBetaAI.java
+++ MiniMaxAlphaBetaAI.java
@@ -69,7 +69,7 @@ public class MiniMaxAlphaBetaAI {
         //exploredChildren++;
         if (depth == maxDepth || !board.legalSpots(playerTurn)) {
             //System.out.println("" + depth + " lag nede i træet er den højeste score for " + Visualizer.turnColor(playerTurn) +", hvis " + Visualizer.turnColor(Board.turnSwitch(playerTurn)) + " spiller perfekt, =" +Board.evaluateScore(board,playerTurn));
-            return Board.boardHeuristic(board);
+            return boardHeuristic(board);
 
         }
         int bestScore = Integer.MIN_VALUE;
@@ -95,7 +95,7 @@ public class MiniMaxAlphaBetaAI {
     private static int minValue(Board board, int playerTurn, int depth, int alpha, int beta) {
         if (depth == maxDepth || !board.legalSpots(playerTurn)) {
             //System.out.println("" + depth + " lag nede i træet er den laveste score for " + Visualizer.turnColor(playerTurn) +", hvis " + Visualizer.turnColor(Board.turnSwitch(playerTurn)) + " spiller perfekt, =" +Board.evaluateScore(board,playerTurn));
-            return Board.boardHeuristic(board);
+            return boardHeuristic(board);
         }
         int bestScore = Integer.MAX_VALUE;
         Board copy = board.copy();
@@ -116,5 +116,138 @@ public class MiniMaxAlphaBetaAI {
         }
         return bestScore;
     }
+
+
+    public static int boardHeuristic(Board board){
+
+        //Parameters
+        int whiteCoins = 0;
+        int blackCoins = 0;
+        int whiteStability = 0;
+        int blackStability = 0;
+        int whiteMobility;
+        int blackMobility;
+        int whiteCorners = 0;
+        int blackCorners = 0;
+
+        //Parameter weights
+        int coinsWeight = 25;
+        int cornerWeight = 500;
+        int stabilityWeight = 30;
+        int mobilityWeight = 5;
+
+        //Scores
+        int coinScore = 0;
+        int mobilityScore = 0;
+        int stabilityScore = 0;
+        int cornerScore = 0;
+        int score;
+
+        //Runs through all cells on board and counts parameters
+        for(int x=0; x<board.x_axis; x++){
+            for(int y=0; y<board.y_axis; y++){
+                if(board.map[x][y]==1){
+                    whiteCoins++;
+                    if(isStable(board, x, y, 1)){
+                        whiteStability++;
+                    }
+                    if((x == 0 || x == board.x_axis - 1) && (y == 0 || y == board.y_axis - 1)){
+                        whiteCorners++;
+                    }
+                }
+
+                if(board.map[x][y]==2){
+                    blackCoins++;
+                    if(isStable(board, x, y, 2)){
+                        blackStability++;
+                    }
+                    if((x == 0 || x == board.x_axis - 1) && (y == 0 || y == board.y_axis - 1)){
+                        blackCorners++;
+                    }
+                }
+            }
+        }
+
+        whiteMobility = getMobility(board,1);
+        blackMobility = getMobility(board,2);
+
+
+        if(whiteCoins+blackCoins != 0) {
+            coinScore = 100 * (whiteCoins - blackCoins) / (whiteCoins + blackCoins);
+        }
+
+        if(whiteMobility+blackMobility != 0) {
+            mobilityScore = 100 * (whiteMobility - blackMobility) / (whiteMobility + blackMobility);
+        }
+
+        if(whiteCorners+blackCorners != 0) {
+            cornerScore = 100 * (whiteCorners - blackCorners) / (whiteCorners + blackCorners);
+        }
+
+        if(whiteStability+blackStability != 0) {
+            stabilityScore = 100 * (whiteStability - blackStability) / (whiteStability + blackStability);
+        }
+
+
+        score = coinsWeight * coinScore + mobilityWeight * mobilityScore + cornerWeight * cornerScore + stabilityWeight * stabilityScore;
+
+        return score;
+    }
+
+
+    //The term mobility refers to the amount of possible moves a player has. The method below returns this number for a given player
+    private static int getMobility(Board board, int playerTurn){
+        int mobility = 0;
+        Board copy = board.copy();
+        copy.legalSpots(playerTurn);
+        for(int i=0; i<copy.x_axis; i++){
+            for(int j=0; j<copy.y_axis; j++){
+                if(copy.map[i][j]==3){
+                    mobility++;
+                }
+            }
+        }
+        return mobility;
+    }
+
+    private static boolean isStable(Board board, int x, int y, int piece) {
+
+        if(!stableDirection(board,x,y,-1,-1,piece)) return false;
+        if(!stableDirection(board,x,y,0,-1,piece)) return false;
+        if(!stableDirection(board,x,y,-1,0,piece)) return false;
+        return stableDirection(board, x, y, 1, -1, piece);
+
+    }
+
+    public static boolean stableDirection(Board board,int x,int y, int dx, int dy, int piece){
+        int l = 1;
+        int end1;
+        int end2 = 10;
+        while (board.isOnBoard(x + l * dx, y + l * dy) && board.map[x + l * dx][y + l * dy] == piece) {
+
+            l++;
+        }
+        if (board.isOnBoard(x + l * dx, y + l * dy)) {
+            end1 = board.map[x + l * dx][y + l * dy];
+
+            dx *= -1;
+            dy *= -1;
+            l = 1;
+            while (board.isOnBoard(x + l * dx, y + l * dy) && board.map[x + l * dx][y + l * dy] == piece) {
+                l++;
+            }
+            if (board.isOnBoard(x + l * dx, y + l * dy)) {
+                end2 = board.map[x + l * dx][y + l * dy];
+            }
+            return end2 == 10 || end1 == end2;
+        }
+        return true;
+    }
+
+
+
+
+
+
 
 }
