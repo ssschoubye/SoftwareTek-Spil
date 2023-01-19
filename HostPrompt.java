766045723030933ce33c03ff7f3118158fb2ecbb
ssschoubye <116802763+ssschoubye@users.noreply.github.com>
2023-01-19 12:31:00 +0100

    Update HostPrompt.java

--- HostPrompt.java
+++ HostPrompt.java
@@ -12,18 +12,14 @@ import java.net.InetAddress;
 import java.util.Objects;
 
 public class HostPrompt {
-    //Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(HostPrompt.class.getResource("OnlineDesign.fxml"))));
-    int dim;
-
-    static Stage stage = new Stage();
-    public void runHostPrompt() throws IOException {
-
+    static Stage stage = new Stage(); //static so that it can be accessed from other classes
+    public void runHostPrompt() throws IOException {//Launches the stage further explanation of stages and scenes can be found in the menu class.
         Parent root = FXMLLoader.load(getClass().getResource("OnlineDesign.fxml"));
         Scene scene = new Scene(root);
-        IPhost = (Label) scene.lookup("#IPhost");
-        InetAddress localHost = InetAddress.getLocalHost();
-        String hostip = localHost.getHostAddress();
-        setIP(hostip);
+        IPhost = (Label) scene.lookup("#IPhost"); //The IPhost label is found in the FXML file and is assigned to the IPhost variable.
+        InetAddress localHost = InetAddress.getLocalHost(); //The localHost variable is assigned the local host address.
+        String hostip = localHost.getHostAddress(); //The hostip variable is assigned the local host address.
+        setIP(hostip); //Sets the text of the IPhost label to the local host address.
 
         stage.setScene(scene);
         stage.show();
@@ -31,37 +27,36 @@ public class HostPrompt {
 
 
     @FXML
-    Button button;
+    Button button; //This corresponds to a button named button in the FXML file.
     @FXML
-    private TextField IPjoin;
-    public static String IPinput;
+    private TextField IPjoin; //This corresponds to a text field named IPjoin in the FXML file.
+    public static String IPinput; //This is the variable that will be used to store the IP address that the user inputs.
 
     public static boolean firstTime = true;
     public void joinGame() throws IOException, ClassNotFoundException, InterruptedException {
+        //The joinGame is called through the onAction function of the button in the FXML file.
         IPinput = IPjoin.getText(); //Gets the IP address input.
         Stage stage = (Stage) button.getScene().getWindow();//Close the stage and the start the controller.
         stage.close();
-        boolean isHost = false;
         PlayOnlineClient playOnlineClient = new PlayOnlineClient();
         playOnlineClient.gameStart();
 
     }
 
     @FXML
-    private Label IPhost;
+    private Label IPhost; //The IPhost label is found in the FXML file and is assigned to the IPhost variable.
 
     @FXML
-    private void setIP(String hostip){IPhost.setText(hostip);}
+    private void setIP(String hostip){IPhost.setText(hostip);} //Sets the text of the IPhost label to the local host address.
 
     @FXML
     private Button hostgame;
     @FXML
     private void startHost() throws IOException, ClassNotFoundException, InterruptedException {
-        boolean isHost = true;
-        Stage stage = (Stage) hostgame.getScene().getWindow();//Close the stage and the start the controller.
+        Stage stage = (Stage) hostgame.getScene().getWindow();//Finds the stage that the button
         stage.close();
-        PlayOnlineHost playOnlineHost = new PlayOnlineHost();
-        playOnlineHost.gameStart();
+        PlayOnlineHost playOnlineHost = new PlayOnlineHost(); //Initialises an object of the type "PlayOnlineHost".
+        playOnlineHost.gameStart();// Starts the host controller.
     }
 
     public static String getIPinput(){
